c1e9559444b14747d6d11d590558bd6860c6b90d
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2013-01-17 00:21:19 +0100

    Add test deploy build files.

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
 handled/handled.js dev/index.html &&
+mv index.handled.html index.html &&
+echo "Renamed handled file to index.html" &&
 rm -rf img/* && cp dev/img/* img &&
-mv index.handled.html index.html
+echo "Copied all images."
+echo "Done!"
