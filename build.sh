1e714203d71d02428efd04b254dfe6cabe9d4ecd
Markus Amalthea Magnuson <markus.magnuson@gmail.com>
2013-01-20 17:53:14 +0100

    Add a comment in build.sh.

--- build.sh
+++ build.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
+# This build script will process the contents of the dev directory and output
+# the resulting files in the root directory, which is where the deployed
+# application lives.
+
 handled/handled.js dev/index.html &&
 mv index.handled.html index.html &&
 echo "Renamed handled file to index.html" &&
